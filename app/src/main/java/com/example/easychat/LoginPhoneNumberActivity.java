4611e639fcaa97ae1ad74df0230e86810940da0a
Shoab <shoabahamed2007015@gmail.com>
2024-05-13 20:40:40 +0600

    complete Phone Login Page UI Design

---
+++ app/src/main/java/com/example/easychat/LoginPhoneNumberActivity.java
@@ -0,0 +1,22 @@
+package com.example.easychat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginPhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_phone_number);
+
+
+    }
+}
